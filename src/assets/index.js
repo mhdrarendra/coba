130dd4a8051e869ccfac3b2ea3e8123f97bab117
mhdrarendra <mhd.rarendra@gmail.com>
2025-02-17 20:08:49 +0700

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,49 @@
+
+import picture1 from "./IMG-20250216-WA0021.jpg";
+import picture2 from "./IMG-20250216-WA0025.jpg";
+import picture3 from "./IMG-20250216-WA0026.jpg";
+import picture4 from "./IMG-20250216-WA0027.jpg";
+import picture5 from "./IMG-20250216-WA0028.jpg";
+import picture6 from "./IMG-20250216-WA0029.jpg";
+import picture7 from "./IMG-20250216-WA0033.jpg";
+import aquarium1 from "./aquarium1.jpg";
+import aurora2 from "./aurora2.jpg";
+
+import seasons from "./seasons.jpg";
+import lany from "./lany.png";
+import onbended from "./onbended.jpg";
+import herewithme from "./herewithme.jpg";
+import givemeyour from "./givemeyour.jpg";
+import honey from "./honey.jpg";
+import always from "./always.jpg";
+import mylove from "./mylove.jpg";
+
+
+import fireworks from "./fireworks.gif";
+import mocha from "./mocha.gif";
+
+
+
+
+export {
+
+picture1,
+picture2,
+picture3,
+picture4,
+picture5,
+picture6,
+picture7,
+aquarium1,
+aurora2,
+seasons,
+mocha,
+fireworks,
+lany,
+onbended,
+herewithme,
+givemeyour,
+honey,
+always,
+mylove,
+};
